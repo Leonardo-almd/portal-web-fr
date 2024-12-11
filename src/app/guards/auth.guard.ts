009aad3981e0be609ceda8e8029239038fee0325
Leonardo Almeida <leonardo.alv1@hotmail.com>
2024-12-11 13:20:12 -0300

    fix-authGuard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -6,15 +6,8 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
   if (authService.isLoggedIn()) {
-    if(state.url === '/auth') {
-      router.navigate(['/']);
-    }
-    return true;
+    return state.url === '/auth' ? router.createUrlTree(['/']) : true;
   } else {
-    if(state.url !== '/auth') {
-      return false
-    } else {
-      return true;
-    }
+    return state.url !== '/auth' ? router.createUrlTree(['/auth']) : true;
   }
 };
