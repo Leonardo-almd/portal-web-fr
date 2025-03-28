42356277e8aefb4562b9b28a4baf814e0312898e
Leonardo Almeida <leonardo.alv1@hotmail.com>
2025-03-27 23:01:47 -0300

    fix-removeFirebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,6 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PoHttpRequestModule } from '@po-ui/ng-components';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
@@ -15,17 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     importProvidersFrom([BrowserAnimationsModule, PoHttpRequestModule]),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'portal-web-fr',
-        appId: '1:11838731901:web:a4b1a1b6cc088e1b7a2314',
-        storageBucket: 'portal-web-fr.appspot.com',
-        apiKey: 'AIzaSyAjdDrZe3lbxNSvsJpBV1u29pzRJJCPOlQ',
-        authDomain: 'portal-web-fr.firebaseapp.com',
-        messagingSenderId: '11838731901',
-      })
-    ),
-    provideAuth(() => getAuth()),
     provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
